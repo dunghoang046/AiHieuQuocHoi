96bc06865242be88c97852f2649e3bd331c5caa9
dunghoang046 <hoangthuc046@gmail.com>
2022-04-29 16:58:12 +0700

    cm maint

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,49 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<audio_session/AudioSessionPlugin.h>)
+#import <audio_session/AudioSessionPlugin.h>
+#else
+@import audio_session;
+#endif
+
+#if __has_include(<connectivity/FLTConnectivityPlugin.h>)
+#import <connectivity/FLTConnectivityPlugin.h>
+#else
+@import connectivity;
+#endif
+
+#if __has_include(<just_audio/JustAudioPlugin.h>)
+#import <just_audio/JustAudioPlugin.h>
+#else
+@import just_audio;
+#endif
+
+#if __has_include(<path_provider_ios/FLTPathProviderPlugin.h>)
+#import <path_provider_ios/FLTPathProviderPlugin.h>
+#else
+@import path_provider_ios;
+#endif
+
+#if __has_include(<shared_preferences_ios/FLTSharedPreferencesPlugin.h>)
+#import <shared_preferences_ios/FLTSharedPreferencesPlugin.h>
+#else
+@import shared_preferences_ios;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AudioSessionPlugin registerWithRegistrar:[registry registrarForPlugin:@"AudioSessionPlugin"]];
+  [FLTConnectivityPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTConnectivityPlugin"]];
+  [JustAudioPlugin registerWithRegistrar:[registry registrarForPlugin:@"JustAudioPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
+}
+
+@end
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,17 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\SETUP\\Flutter\\flutter_windows_2.10.1-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\TinDucSoftWare\\2021\\App_AiHieuQH\\source\\app_aihieuquochoi" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\SETUP\\Flutter\\flutter_windows_2.10.1-stable\\flutter"
+  "PROJECT_DIR=D:\\TinDucSoftWare\\2021\\App_AiHieuQH\\source\\app_aihieuquochoi"
+  "FLUTTER_ROOT=D:\\SETUP\\Flutter\\flutter_windows_2.10.1-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\TinDucSoftWare\\2021\\App_AiHieuQH\\source\\app_aihieuquochoi\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\TinDucSoftWare\\2021\\App_AiHieuQH\\source\\app_aihieuquochoi"
+  "FLUTTER_TARGET=lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=D:\\TinDucSoftWare\\2021\\App_AiHieuQH\\source\\app_aihieuquochoi\\.dart_tool\\package_config.json"
+)
